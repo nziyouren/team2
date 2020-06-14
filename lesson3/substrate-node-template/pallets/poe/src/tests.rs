5630b14fe74482311d634dae9e3b073d03de2101
nziyouren <nziyouren@gmail.com>
2020-06-14 12:15:08 +0800

    happy path

--- lesson3/substrate-node-template/pallets/poe/src/tests.rs
+++ lesson3/substrate-node-template/pallets/poe/src/tests.rs
@@ -1,5 +1,16 @@
 // Tests to be written here
 
 use crate::{Error, mock::*};
+use super::*;
 use frame_support::{assert_ok, assert_noop};
+
+
+#[test]
+fn create_claim_work() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0,1,2,3];
+        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+        assert_eq!(Proofs::<Test>::get(&claim), (1, system::Module::<Test>::block_number()));
+    })
+}
 
