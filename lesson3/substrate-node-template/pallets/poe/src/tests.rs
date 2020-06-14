647a9676532e29c029ba2a233bad7bf075842092
nziyouren <nziyouren@gmail.com>
2020-06-14 13:31:13 +0800

    add revoke claim test

--- lesson3/substrate-node-template/pallets/poe/src/tests.rs
+++ lesson3/substrate-node-template/pallets/poe/src/tests.rs
@@ -8,7 +8,7 @@ use frame_support::{assert_ok, assert_noop};
 #[test]
 fn create_claim_work() {
     new_test_ext().execute_with(|| {
-        let claim = vec![0,1,2,3];
+        let claim = vec![0, 1, 2, 3];
         assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
         assert_eq!(Proofs::<Test>::get(&claim), (1, system::Module::<Test>::block_number()));
     })
@@ -18,7 +18,7 @@ fn create_claim_work() {
 #[test]
 fn create_claim_alreay_existed() {
     new_test_ext().execute_with(|| {
-        let claim = vec![0,1,2,3];
+        let claim = vec![0, 1, 2, 3];
         //insert first time
         let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
 
@@ -31,8 +31,45 @@ fn create_claim_alreay_existed() {
 #[test]
 fn create_claim_too_long() {
     new_test_ext().execute_with(|| {
-        let claim = vec![0,1,2,3,4,5,6];
+        let claim = vec![0, 1, 2, 3, 4, 5, 6];
         assert_noop!(PoeModule::create_claim(Origin::signed(1), claim.clone()), Error::<Test>::ProofTooLong);
+    })
+}
+
+
+//test case for revoke claim
+#[test]
+fn revoke_claim_ok() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1, 2, 3];
+        //insert first time
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+        assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim.clone()));
+    })
+}
+
+
+//test case for revoke claim not exsited
+#[test]
+fn revoke_claim_not_existed() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1, 2, 3];
+        //insert first time
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+        let another_claim = vec![4, 4, 4, 4];
+        assert_noop!(PoeModule::revoke_claim(Origin::signed(1), another_claim.clone()), Error::<Test>::ClaimNotExist);
+    })
+}
+
+//test case for revoke claim not owner
+#[test]
+fn revoke_claim_not_owner() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1, 2, 3];
+        //insert first time
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+        assert_noop!(PoeModule::revoke_claim(Origin::signed(2), claim.clone()), Error::<Test>::NotClaimOwner);
     })
 }
 
